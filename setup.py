d220f683a653624850daba6995b69d394013ad9e
Simone <simone.compri@gmail.com>
2023-06-29 15:01:19 +0200

    Base structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='volleyball_blocking_multimodal_training',
+    version='0.1.0',
+    description='',
+    author='Simone Compri',
+    author_email='simone.compri@studenti.unitn.it',
+    url='https://github.com/S0n0i0/volleyball_blocking_multimodal_training',
+    packages=find_packages(),
+    install_requires=[],
+)
